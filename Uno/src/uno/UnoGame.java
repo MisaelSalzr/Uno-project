4e081fc6425ba31db8c5380a1a5d4379e8614873
MisaelSalzr <55956701+MisaelSalzr@users.noreply.github.com>
2021-10-07 15:28:59 -0400

    Add files via upload

--- Uno/src/uno/UnoGame.java
+++ Uno/src/uno/UnoGame.java
@@ -1,6 +1,7 @@
 package uno;
 
 import Deck.Card;
+import static Deck.Colors.SPECIAL;
 import Deck.Deck;
 import Player.Player;
 import java.util.*;
@@ -11,11 +12,13 @@ import java.util.*;
  */
 public class UnoGame {
     
-    private Deck gameDeck;
-    private Player [] players;
-    private Stack<Card> playedPile = new Stack<>();
+    private static Deck gameDeck;
+    private static Player [] players;
+    private static Stack<Card> playedPile = new Stack<>();
+    
+    int playersTurn;
     private Scanner in = new Scanner(System.in);
-    char [] gameState;
+    char gameState;
     
     UnoGame(int playerCount){
         
@@ -24,19 +27,62 @@ public class UnoGame {
         
         for(int i = 0; i < playerCount; i++){
         
+            players[i] = new Player();
             players[i].initHand(gameDeck);
             
         }
         
         System.out.print("Choose: Q to quit, R to reset, or P to play! ");
-        gameState = in.next().toCharArray();
+        gameState = in.next().charAt(0);
+        
+        switch(gameState){
+        
+            case 'Q':
+                break;
+                
+            case 'R':
+                ResetGame();
+                break;
+            
+            case 'P':
+                break;
+                
+            default:
+                System.out.println("Input unknown, terminating program");
+                return;
+            
+        }
         
         
     }
     
-    void ResetGame(){
+    static void ResetGame(){
         
         gameDeck = new Deck();
+        
+    }
+    
+    static void playGame(){
+    
+        
+    }
+    
+    static boolean isUno(int playerTurn){
+        return (players[playerTurn].gethandSize() == 0);
+    }
+    
+    static boolean isValidCard(Card playedCard){
+    
+        if(playedCard.getCardColor() == SPECIAL)
+            return true;
+        
+        if((playedPile.peek().getCardColor() == playedCard.getCardColor()))
+            return true;
+        
+        if(playedPile.peek().getCardValue() == playedCard.getCardValue())
+            return true;
+        
+        return false;
         
     }
     
